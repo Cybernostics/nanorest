66d93ebe5d1216d7cc5875845bd2cf2326756145
jason <jason@joeymail.net>
2015-01-31 06:51:48 +1100

    Added postEntity client query test

--- src/test/java/com/cybernostics/nanorest/test/client/HttpRequestExecutorTest.java
+++ src/test/java/com/cybernostics/nanorest/test/client/HttpRequestExecutorTest.java
@@ -2,7 +2,15 @@ package com.cybernostics.nanorest.test.client;
 
 import static org.junit.Assert.assertThat;
 
-import org.hamcrest.core.Is;
+import java.util.Arrays;
+import java.util.Collections;
+
+import javassist.expr.NewArray;
+
+import org.apache.commons.collections.ListUtils;
+
+import static org.hamcrest.core.Is.is;
+
 import org.hamcrest.core.IsNot;
 import org.hamcrest.core.IsNull;
 import org.junit.Before;
@@ -25,11 +33,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import com.cybernostics.nanorest.client.HttpRequestExecutor;
 import com.cybernostics.nanorest.client.CallableHttpService;
 import com.cybernostics.nanorest.client.NanoRestClientConfig;
+import com.cybernostics.nanorest.example.api.v1.Greeting;
 import com.cybernostics.nanorest.example.api.v1.GreetingsService;
 import com.cybernostics.nanorest.lib.interfaceparsers.RequestSpecification;
 import com.cybernostics.nanorest.servicelocator.DefaultServiceEndpoint;
 import com.cybernostics.nanorest.servicelocator.RemoteServiceEndpoint;
 import com.cybernostics.nanorest.servicelocator.ServiceEndpointDirectory;
+import com.google.common.collect.Collections2;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration( classes = {HttpRequestExecutorTest.testConfig.class,NanoRestClientConfig.class})
@@ -40,6 +50,7 @@ public class HttpRequestExecutorTest {
 	private MockCallableHttpService mockService;
 	private RemoteServiceEndpoint endpoint;
 	private HttpRequestExecutor httpRequestExecutor;
+
 
 	public static class MockCallableHttpService extends CallableHttpService{
 		public String urlstring = null;
@@ -61,7 +72,7 @@ public class HttpRequestExecutorTest {
 	public void setup() {
 		mockService = new MockCallableHttpService();
 		endpoint = directory.getEndpoint(GreetingsService.class);
-		assertThat(endpoint, Is.is(IsNot.not(IsNull.nullValue())));
+		assertThat(endpoint, is(IsNot.not(IsNull.nullValue())));
 		httpRequestExecutor = new HttpRequestExecutor();
 	}
 
@@ -73,8 +84,26 @@ public class HttpRequestExecutorTest {
 		.withBodyIndex(-1);
 
 		httpRequestExecutor.doRequest(endpoint,mockService, requestSpecification, new Object[0]);
-		assertThat(mockService.urlstring, Is.is("/sample"));
-		assertThat(mockService.method, Is.is(HttpMethod.GET));
+		assertThat(mockService.urlstring, is("/sample"));
+		assertThat(mockService.method, is(HttpMethod.GET));
+	}
+
+	@Test
+	public void postXYZ() {
+		Greeting postEntityDTO = new Greeting(1,"putContent", "putDescription");
+		Object[] agsObjects = Arrays.asList(postEntityDTO).toArray();
+
+		RequestSpecification requestSpecification = new RequestSpecification()
+		.appendURL("/sample")
+		.withHttpRequestMethod(HttpMethod.POST)
+		.withBodyIndex(0);
+
+		httpRequestExecutor.doRequest(endpoint,mockService, requestSpecification, agsObjects);
+		assertThat(mockService.urlstring, is("/sample"));
+		assertThat(mockService.method, is(HttpMethod.POST));
+		byte[] body = mockService.entity.getBody();
+		System.out.println(new String(body));
+		assertThat(body, is("{\"id\":1,\"content\":\"putContent\",\"description\":\"putDescription\"}".getBytes()));
 	}
 
 
